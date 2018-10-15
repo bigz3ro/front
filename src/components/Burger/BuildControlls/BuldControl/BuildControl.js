a4136567b509804712d8ceee600360bb7607e517
Your Name <skyblack1401@gmail.com>
2018-10-15 17:12:06 +0700

    add new

--- src/components/Burger/BuildControlls/BuldControl/BuildControl.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import classes from './BuildControl.css';
-
-const BuildControl = (props) => {
-
-}
---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './BuildControl.css';
+
+const BuildControl = (props) => (
+    <div>
+        <div className={ classes.Label }>{ props.label }</div>
+        <button className={ classes.More }>More</button>
+        <button className={ classes.Less }>Less</button>
+    </div>
+)
+
+export default BuildControl;
